2ae8219d009ae8801791b93633ecc7158f9466ba
hetsketch <al3xander.koval@gmail.com>
2016-07-30 17:53:03 +0300

    .gitignore added

---
+++ src/ru/forque/singleton/Singleton.java
@@ -0,0 +1,8 @@
+package ru.forque.singleton;
+
+/**
+ * @author hetsketch
+ * @date 7/30/16
+ */
+public class Singleton {
+}
---
+++ src/ru/forque/singleton/ThreadSafeSingleton.java
@@ -0,0 +1,8 @@
+package ru.forque.singleton;
+
+/**
+ * Author: hetsketch
+ * Created: 7/30/16
+ */
+public class ThreadSafeSingleton {
+}
